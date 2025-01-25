8c38d77f7ed6f962550e6849634b86a97bd87d6e
Minerva Yan <xitingyan99@gmail.com>
2025-01-24 21:42:11 -0500

    Added option to skip full breakdown.

--- diceRoller.c
+++ diceRoller.c
@@ -7,19 +7,26 @@ int main(void) {
     const int diceArray[] = {4, 6, 8, 10, 12, 20, 100}; //This array holds the types of dice that can be rolled.
     int diceCount[] = {0, 0, 0, 0, 0, 0, 0}; //This array will holds the amount of times each type of dice is going to be rolled as requested by the user.
     int i, finalSum = 0, diceSum = 0; //diceSum holds the sum of all dice rolls of a certain type. finalSum holds the final total of all dice rolled.
+    char justSum;
 
     for (i = 0; i < 7; i++) { //Requests amount of times each type of dice will be rolled. Stores in diceCount.
         printf("Amount of d%d rolls: ", diceArray[i]);
         scanf("%d", &diceCount[i]);
     }
 
+    getchar();
+    printf("Do you want a full breakdown? Y/N: "); //Individual dice rolls and dice type sums will not be printed if user does not request it.
+    scanf("%c", &justSum);
+
     for (i = 0; i < 7; i++) { //Rolls the dice and prints the value. Keeps rolling until the dice roll count for that type is exhausted. Rolls the next type.
         for (;diceCount[i] > 0; diceCount[i] = diceCount[i] - 1) {
             int randValue = rand() % diceArray[i] + 1;
-            printf("d%d Roll: %d\n", diceArray[i], randValue);
+            if (justSum == 'Y' || justSum == 'y') {
+                printf("d%d Roll: %d\n", diceArray[i], randValue);
+            }
             diceSum = diceSum + randValue; //Calculates the diceSum on a rolling basis.
         }
-        if (diceSum != 0) {
+        if (diceSum != 0 && (justSum == 'Y' || justSum == 'y')) {
             printf("Sum of d%d Rolls: %d\n", diceArray[i], diceSum); //Prints the sum of all dice rolled for the given dice type.
         }
         finalSum = finalSum + diceSum; //Calculates the finalSum on a rolling basis.
